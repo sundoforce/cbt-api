ab5eac4fc38381157c8c1364e1f23dc6b3008ce7
SDK <admin@sundo.kim>
2023-01-11 15:01:25 +0000

    gorilla

--- gorilla.go
+++ gorilla.go
@@ -1,70 +1,70 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/lib/pq"
 )
 
-type User struct {
-	gorm.Model
-	Name  string
-	Email string
-}
-
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/{table}/{id}", getRecord).Methods("GET")
+	err := godotenv.Load()
+    if err != nil {
+      log.Fatal("Error loading .env file")
+    }
+	db, err := sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable",
+		os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME")))
+    if err != nil {
+    	log.Fatal(err)
+    }
+    defer db.Close()
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+    r := mux.NewRouter()
+
+	r.HandleFunc("/api/{table}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		tableName := vars["table"]
+
+		rows, err := db.Query(fmt.Sprintf("SELECT * FROM %s", tableName))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		defer rows.Close()
+
+		var data []map[string]interface{}
+		for rows.Next() {
+			columns, _ := rows.Columns()
+			count := len(columns)
+			values := make([]interface{}, count)
+			valuePtrs := make([]interface{}, count)
+			for i := 0; i < count; i++ {
+				valuePtrs[i] = &values[i]
+			}
+			rows.Scan(valuePtrs...)
+
+			entry := make(map[string]interface{})
+			for i, col := range columns {
+				var v interface{}
+				val := values[i]
+				b, ok := val.([]byte)
+				if ok {
+					v = string(b)
+				} else {
+					v = val
+				}
+				entry[col] = v
+			}
+			data = append(data, entry)
+		}
+
+		json.NewEncoder(w).Encode(data)
+	}).Methods("GET")
+
+    log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-func getRecord(w http.ResponseWriter, r *http.Request) {
-	db, err := gorm.Open("postgres", "host=localhost user=gorm dbname=gorm password=gorm sslmode=disable")
-	if err != nil {
-		fmt.Fprintln(w, "Error connecting to the database:", err)
-		return
-	}
-	defer db.Close()
 
-	vars := mux.Vars(r)
-	tableName := vars["table"]
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		fmt.Fprintln(w, "Invalid record ID")
-		return
-	}
-
-	var record interface{}
-	switch tableName {
-	case "apple":
-		record = &Apple{}
-	case "google":
-		record = &Google{}
-	default:
-		fmt.Fprintln(w, "Invalid table name")
-		return
-	}
-
-	if err := db.Table(tableName).First(record, id).Error; err != nil {
-		fmt.Fprintln(w, "Error fetching record:", err)
-		return
-	}
-
-	// TODO: print out the record
-}
-
-type Apple struct {
-	gorm.Model
-	Name string
-}
-
-type Google struct {
-	gorm.Model
-    Name string
-}
